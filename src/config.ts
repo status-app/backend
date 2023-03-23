cf0e2b11408d43d8edc49e01f822ef66de3b1f88
lambdagg <lambdagg@tuta.io>
2023-03-23 16:28:13 +0100

    added command-line option `-e` for the environment file

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,12 @@
 import { config } from "dotenv";
-let _loaded = false; !_loaded && (config() || true) && (_loaded = true);
+import minimist from "minimist";
+
+let argv = null;
+!argv && (() => {
+  argv = minimist(process.argv.slice(2));
+  // pnpm start -e xyz.env
+  config({ path: argv["e"] || ".env" });
+})();
 
 const logLevel = process.env.DEBUG ? 4 : parseInt(process.env.LOG_LEVEL || "1", 10);
 
